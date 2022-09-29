07f2187e4bc2051d288206398e725d9e4b6093f1
Xuan Tan <x_tan2@u.pacific.edu>
2022-09-28 17:00:39 -0700

    xt

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -16,7 +16,7 @@ public class SimplePicture extends GraphicsProgram {
 		GImage robot = new GImage(, 200, 200);
 		add(robot);
 
-		GLabel label = new GLabel("Xuantan did it", 200, 400);
+		GLabel label = new GLabel("Tanxuan did it", 200, 400);
 		add(label);
 	}	
 	
